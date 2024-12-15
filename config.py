cf7f2bbd877cc23cd7ded9e6f6d1982139f6d045
Mr AK <abcdwalaokk@gmail.com>
2024-12-15 09:03:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config(object):
     pass
     """
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7350085099:AAHQ6uYn3uFPpf0o4p_BrDrf9MM4Fz41iSU")
-    API_ID = int(os.environ["API_ID", 26391910"]
+    API_ID = int(os.environ["API_ID", "26391910"]
     API_HASH = os.environ["API_HASH", "d351b6fa9818bef784ac816baa4cb049"]
     AUTH_USERS = "502980590"""
     WEBHOOK = True  # Don't change this
