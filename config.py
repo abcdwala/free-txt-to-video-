41fcbf56aaa3fb57ba3c6967c85aaeaf6e36c87c
Mr AK <abcdwalaokk@gmail.com>
2024-12-15 08:55:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7428627366:AAHhTXhxSObHQVUv-VLxbDz8ik4YRtIoK6M")
-    API_ID = int(os.environ["API_ID", 23031620]
-    API_HASH = os.environ["API_HASH", "31cb00c1cbe580394778b43105864bca"]
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7350085099:AAHQ6uYn3uFPpf0o4p_BrDrf9MM4Fz41iSU")
+    API_ID = int(os.environ["API_ID", 26391910"]
+    API_HASH = os.environ["API_HASH", "d351b6fa9818bef784ac816baa4cb049"]
     AUTH_USERS = "502980590"""
     WEBHOOK = True  # Don't change this
     PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
